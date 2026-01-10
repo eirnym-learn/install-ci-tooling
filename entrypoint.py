34d3425a0347f350adf42f54d50e1d46b7f7308e
Eir Nym <485399+eirnym@users.noreply.github.com>
2026-01-10 16:21:16 +0100

    Initial implementation

---
+++ entrypoint.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+from collections.abc import Iterator
+import argparse
+import logging
+import os
+import re
+import subprocess
+import sys
+import tomllib
+from typing import Any, Callable
+
+logger = logging.getLogger(__name__)
+
+# normal for Rust crates, quite restrictive for others
+VERSION_RE = re.compile(r"\d+\.\d+\.\d+")
+TOOL_NAME_RE = re.compile(r"[a-zA-Z0-9][a-zA-Z0-9_-]+")
+KNOWN_DATA_SOURCES = ("crate",)
+KNOWN_LOG_LEVELS = ("error", "warn", "info", "debug")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Simple install for CI tooling")
+    parser.add_argument(
+        "--cargo-binstall",
+        help="Use `cargo binstall` to install Rust tools",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--default-datasource",
+        help="Default data source",
+    )
+    parser.add_argument("--force", help="Force reinstall", action="store_true")
+    parser.add_argument(
+        "--log-level",
+        help="Set log level",
+        choices=KNOWN_LOG_LEVELS,
+        default="information",
+    )
+    parser.add_argument("-n", "--dry-run", help="Dry run", action="store_true")
+    parser.add_argument(
+        "toml_file",
+        help="Path to the TOML file containing tool versions",
+        metavar="tools-file",
+    )
+    parser.add_argument("section", help="Section to use")
+    return parser.parse_args()
+
+
+def setup_logging(log_level: str | None):
+    match log_level:
+        case "error":
+            logging_level = logging.ERROR
+        case "warn":
+            logging_level = logging.WARNING
+        case "info":
+            logging_level = logging.INFO
+        case "debug":
+            logging_level = logging.DEBUG
+        case _:
+            logging_level = logging.INFO
+
+    format = "%(asctime)s [%(levelname)s] %(message)s"
+    logging.basicConfig(level=logging_level, format=format)
+
+
+def validate_version(tool_name: str, version: Any) -> bool:
+    """Sanitizes version input.
+
+    Returns `None` if version isn't a string or doesn't match `VERSION_RE`
+    """
+    if version is None:
+        logger.error(f"{tool_name}: Version is mandatory")
+        return False
+
+    if not isinstance(version, str):
+        logger.error(f"{tool_name}: Version must be a string")
+        return False
+
+    if VERSION_RE.fullmatch(version) is None:
+        logger.error(f"{tool_name}: Version doesn't match semver scheme: {version!r}")
+        return False
+
+    return True
+
+
+def validate_tool_name(tool_name: str) -> bool:
+    """Sanitizes tool name input.
+
+    Returns `None` if tool name doesn't match the `TOOL_NAME_RE`.
+    """
+    tool_name = tool_name.strip()
+    if TOOL_NAME_RE.fullmatch(tool_name) is None:
+        logger.error(f"Tool name is unexpected: {tool_name!r}")
+        return False
+
+    return True
+
+
+def validate_datasource(tool_name: str, datasource: Any) -> bool:
+    """Sanitizes datasource input.
+
+    Returns `None` if datasource is not a string.
+    """
+    if datasource is None:
+        return True
+
+    if not isinstance(datasource, str):
+        logger.error(f"{tool_name}: Datasource is not a string")
+        return False
+
+    return True
+
+
+def read_tools(
+    toml_file: str, section: str, default_datasource: str | None
+) -> list[tuple[str, Any, Any]] | None:
+    """Reads given section from given tools file.
+
+    Returns tuple of (tool name, version, datasource).
+
+    default_datasource is used if datasource is not defined or invalid.
+    """
+    if not os.path.exists(toml_file):
+        logger.error("Tools file doesn't exist")
+        return None
+    with open(toml_file) as f:
+        data = tomllib.loads(f.read())
+
+    tools: dict[str, Any] | None = data.get(section)
+
+    if tools is None or not isinstance(tools, dict):  # pyright: ignore[reportUnnecessaryIsInstance]
+        logger.error(f"Tools file doesn't contain section requested: {section}")
+        return None
+
+    result: list[tuple[str, str, str]] = []
+
+    for tool_name, value in tools.items():
+        if not validate_tool_name(tool_name):
+            return None
+
+        if isinstance(value, str):  # value is version
+            version = value
+            datasource = default_datasource
+        elif isinstance(value, dict):
+            version = value.get("version", None)
+            datasource = value.get("datasource", default_datasource)
+        else:
+            logger.error(f"{tool_name}: Value must be a string or dict")
+            return None
+
+        if not validate_version(tool_name, version):
+            return None
+
+        if not validate_datasource(tool_name, datasource):
+            return None
+
+        if datasource is None or datasource not in KNOWN_DATA_SOURCES:
+            logger.warning(f"{tool_name}: Datasource is not supported")
+            continue
+
+        result.append((tool_name, version, datasource))
+
+    return result
+
+
+def install_tool_crate(
+    crate_name: str,
+    version: str,
+    force: bool,
+    dry_run: bool,
+    cargo_binstall: bool,
+    installed_crates: dict[Any, Any],
+) -> None:
+    """Install a crate"""
+    if crate_name in installed_crates:
+        installed_version = installed_crates[crate_name]
+
+        if installed_version == version:
+            logger.info(f"{crate_name} already installed at {version}")
+            return
+        else:
+            logger.info(
+                f"{crate_name} version mismatch (found: {installed_version}, expected: {version}), reinstalling..."
+            )
+    else:
+        logger.info(f"> {crate_name} not found, installing...")
+
+    if cargo_binstall:
+        cargo_tool = "binstall"
+    else:
+        cargo_tool = "install"
+
+    command = ["cargo", cargo_tool]
+
+    if cargo_binstall:
+        command.append("--no-confirm")
+
+    if force:
+        command.append("--force")
+    command.append(f"{crate_name}@{version}")
+    logger.info(f"Installing {crate_name}@{version}")
+
+    if dry_run:
+        level = logging.WARN
+    else:
+        level = logging.DEBUG
+
+    if logger.isEnabledFor(level):
+        logger.log(level, f"Running install command: {' '.join(command)!r}")
+
+    if not dry_run:
+        result = subprocess.run(command, check=True, text=True)
+        result.check_returncode()
+
+    logger.info(f"Successfully installed {crate_name} version {version}")
+
+
+DATA_SOURCE_PROCESSORS = {
+    "crate": install_tool_crate,
+}
+
+
+def install_tool(
+    tool_name: str,
+    version: str,
+    datasource: str,
+    force: bool,
+    dry_run: bool,
+    cargo_binstall: bool,
+    installed_crates: dict[Any, Any],
+) -> None:
+    install_function: Callable[..., None] | None = DATA_SOURCE_PROCESSORS.get(
+        datasource
+    )
+
+    if install_function:
+        install_function(
+            tool_name, version, force, dry_run, cargo_binstall, installed_crates
+        )
+
+
+def parse_installed_cargo(crates_toml: str) -> Iterator[tuple[str, str]]:
+    if not crates_toml:
+        return
+
+    if not os.path.exists(crates_toml):
+        # fresh installation
+        return
+
+    with open(crates_toml) as f:
+        data = tomllib.loads(f.read())
+
+    data = data.get("v1")
+
+    if not data:
+        return
+
+    for key in data.keys():
+        (crate, version, _) = key.split(" ", 2)
+        yield (crate, version)
+
+
+def crates_toml():
+    cargo_home = os.environ.get("CARGO_HOME")
+    if not cargo_home:
+        cargo_home = os.path.expanduser("~/.cargo")
+    return os.path.join(cargo_home, ".crates.toml")
+
+
+def main() -> bool:
+    args = parse_args()
+
+    setup_logging(args.log_level)
+    installed_crates = dict(parse_installed_cargo(crates_toml()))
+    logger.debug(f"Installing cargo tools from {args.toml_file}/{args.section}")
+
+    if logger.isEnabledFor(logging.DEBUG):
+        logger.debug("Installed cargo packages:")
+        for crate, version in installed_crates.items():
+            print(f"{crate} {version}")
+    tools = read_tools(args.toml_file, args.section, args.default_datasource)
+    if tools is None:
+        return False
+
+    for tool_name, version, datasource in tools:
+        try:
+            install_tool(
+                tool_name=tool_name,
+                version=version,
+                datasource=datasource,
+                force=args.force,
+                dry_run=args.dry_run,
+                cargo_binstall=args.cargo_binstall,
+                installed_crates=installed_crates,
+            )
+        except subprocess.CalledProcessError:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    if not main():
+        sys.exit(1)
---
+++ tests.py
@@ -0,0 +1,334 @@
+import pathlib
+import subprocess
+import pytest
+import os
+import re
+
+# Minimal .crates.toml to check against
+crates_toml = """
+[v1]
+"cargo-binstall 1.16.6 (registry+https://github.com/rust-lang/crates.io-index)" = ["cargo-binstall"]
+"""
+
+SECTION = "section"
+CRATE_DATASOURCE = "crate"
+SECTION_NEVER_INSTALLED = """
+[section-never-installed]
+never_tool = "1.12.3"
+"""
+
+
+def setup_cargo_home(cargo_home: pathlib.Path, has_crates_toml: bool = True):
+    """Generate cargo home with only .crates_toml file if requested."""
+    cargo_home.mkdir()
+    if has_crates_toml:
+        with open(cargo_home / ".crates.toml", "w") as f:
+            f.write(crates_toml)
+
+
+def setup_tools_toml(
+    tool_file: pathlib.Path,
+    section_data: str,
+    unknown_section: bool = True,
+    raw: bool = False,
+):
+    """Set up tools.toml in temporary file."""
+    with open(tool_file, "w") as f:
+        if raw:
+            f.write(section_data)
+        else:
+            f.write(f"[{SECTION}]\n{section_data}")
+            if unknown_section:
+                f.write(SECTION_NEVER_INSTALLED)
+
+
+def execute_command(
+    cargo_home: pathlib.Path,
+    tool_file: pathlib.Path,
+    cargo_binstall: bool,
+    default_datasource: str | None,
+    force: bool,
+    *args: str,
+):
+    """Execute command in debug mode.
+
+    Returns (execution code, stdout, stderr).
+    """
+    command_args = [
+        "./entrypoint.py",
+        "--log-level=warn",
+        "--dry-run",
+    ]
+
+    if cargo_binstall:
+        command_args.append("--cargo-binstall")
+
+    if force:
+        command_args.append("--force")
+
+    if default_datasource is not None:
+        command_args.append(f"--default-datasource={default_datasource}")
+
+    # it's better to insert this way to avoid ever-shifting argument index
+    command_args.extend(args)
+
+    command_args.extend((str(tool_file), SECTION))
+
+    print(" ".join(command_args))
+
+    env = os.environ.copy()
+    env["CARGO_HOME"] = str(cargo_home)
+    result = subprocess.run(
+        command_args,
+        check=False,
+        capture_output=True,
+        text=True,
+        env=env,
+    )
+
+    return (
+        result.returncode,
+        result.stdout,
+        result.stderr,
+    )
+
+
+@pytest.mark.parametrize("has_crates_toml", (True, False))
+@pytest.mark.parametrize("unknown_section", (True, False))
+@pytest.mark.parametrize("cargo_binstall", (True, False))
+@pytest.mark.parametrize("force", (True, False))
+@pytest.mark.parametrize(
+    "default_datasource,section_data,expected_tools,unsupported_tools",
+    (
+        ("crate", 'install_tool = "1.1.4"', ["install_tool@1.1.4"], []),
+        ("crate", 'install_tool={"version" = "1.1.4"}', ["install_tool@1.1.4"], []),
+        (
+            "crate",
+            'install_tool={"version" = "1.1.4", "datasource" = "crate"}',
+            ["install_tool@1.1.4"],
+            [],
+        ),
+        (
+            "crate",
+            'unknown_tool = {"version" = "1.12.3", "datasource" = "unknown"}',
+            [],
+            ["unknown_tool"],
+        ),
+        (
+            "crate",
+            'install_tool = "1.1.4"\nunknown_tool = {"version" = "1.12.3", "datasource" = "unknown"}',
+            ["install_tool@1.1.4"],
+            ["unknown_tool"],
+        ),
+        (
+            "crate",
+            'install_tool = "1.1.4"\nother_tool = {"version" = "1.12.3"}',
+            ["install_tool@1.1.4", "other_tool@1.12.3"],
+            [],
+        ),
+        ("unknown", 'install_tool = "1.1.4"', [], ["install_tool"]),
+        ("unknown", 'install_tool={"version" = "1.1.4"}', [], ["install_tool"]),
+        (
+            "unknown",
+            'install_tool={"version" = "1.1.4", "datasource" = "crate"}',
+            ["install_tool@1.1.4"],
+            [],
+        ),
+        (
+            "unknown",
+            'unknown_tool = {"version" = "1.12.3", "datasource" = "unknown"}',
+            [],
+            ["unknown_tool"],
+        ),
+        (
+            "unknown",
+            'install_tool = "1.1.4"\nunknown_tool = {"version" = "1.12.3", "datasource" ="unknown"}',
+            [],
+            ["install_tool", "unknown_tool"],
+        ),
+        (
+            "unknown",
+            'install_tool = "1.1.4"\nother_tool = {"version" = "1.12.3"}',
+            [],
+            ["install_tool", "other_tool"],
+        ),
+        (None, 'install_tool = "1.1.4"', [], ["install_tool"]),
+        (None, 'install_tool={"version" = "1.1.4"}', [], ["install_tool"]),
+        (
+            None,
+            'install_tool={"version" = "1.1.4", "datasource"= "crate"}',
+            ["install_tool@1.1.4"],
+            [],
+        ),
+        (
+            None,
+            'unknown_tool = {"version" = "1.12.3", "datasource"= "unknown"}',
+            [],
+            ["unknown_tool"],
+        ),
+        (
+            None,
+            'install_tool = "1.1.4"\nunknown_tool = {"version" = "1.12.3", "datasource"= "unknown"}',
+            [],
+            ["install_tool", "unknown_tool"],
+        ),
+        (
+            None,
+            'install_tool = "1.1.4"\nother_tool = {"version" = "1.12.3"}',
+            [],
+            ["install_tool", "other_tool"],
+        ),
+    ),
+)
+def test_positive(
+    section_data: str,
+    default_datasource: str | None,
+    expected_tools: list[str],
+    unsupported_tools: list[str],
+    has_crates_toml: bool,
+    unknown_section: bool,
+    cargo_binstall: bool,
+    force: bool,
+    tmpdir: pathlib.Path,
+):
+    expected_tools = list(expected_tools)
+    unsupported_tools = list(unsupported_tools)
+    expected_tool_idx = 0
+    unsupported_tool_idx = 0
+    cargo_home = tmpdir / ".cargo"
+    tool_file = tmpdir / "tools.toml"
+    setup_cargo_home(cargo_home, has_crates_toml)
+    setup_tools_toml(tool_file, section_data, unknown_section, raw=False)
+
+    (code, stdout, stderr) = execute_command(
+        cargo_home,
+        tool_file,
+        cargo_binstall,
+        default_datasource,
+        force,
+    )
+    assert len(stdout) == 0
+    assert code == 0
+
+    for line in stderr.splitlines():
+        assert "[ERROR]" not in line
+        if "[DEBUG]" in line or "[INFO]" in line:
+            continue
+
+        install_tool = re.sub(
+            "^.* \\[WARNING] Running install command: '(.*)'$", "\\1", line
+        )
+        unsupported = re.sub(
+            "^.* \\[WARNING] ([^:]+): Datasource is not supported$", "\\1", line
+        )
+        if install_tool == line:
+            install_tool = None
+        if unsupported == line:
+            unsupported = None
+
+        if install_tool:
+            cmd = install_tool.split()
+
+            assert len(expected_tools) > expected_tool_idx, expected_tools
+            expected_tool = expected_tools[expected_tool_idx]
+            expected_tool_idx += 1
+
+            assert cmd[0] == "cargo"
+            if cargo_binstall:
+                assert cmd[1] == "binstall"
+            else:
+                assert cmd[1] == "install"
+
+            idx = 2
+            if cargo_binstall:
+                assert cmd[idx] == "--no-confirm"
+                idx += 1
+
+            if force:
+                assert cmd[idx] == "--force"
+                idx += 1
+
+            assert cmd[idx] == expected_tool
+
+        if unsupported:
+            assert len(unsupported_tools) > unsupported_tool_idx, unsupported_tools
+            assert unsupported == unsupported_tools[unsupported_tool_idx]
+            unsupported_tool_idx += 1
+
+
+@pytest.mark.parametrize(
+    "section_data,expected_error",
+    (
+        # tool name is invalid
+        ('-invalid_tool = "1.2.3"', "Tool name is unexpected: '-invalid_tool'"),
+        ('_invalid_tool = "1.2.3"', "Tool name is unexpected: '_invalid_tool'"),
+        ('"invalid_tool*" = "1.2.3"', "Tool name is unexpected: 'invalid_tool*'"),
+        # version name is unexpected or unexpected type
+        ("invalid_tool = true", "invalid_tool: Value must be a string or dict"),
+        ("invalid_tool = false", "invalid_tool: Value must be a string or dict"),
+        ("invalid_tool = 123", "invalid_tool: Value must be a string or dict"),
+        (
+            'invalid_tool = "Wow"',
+            "invalid_tool: Version doesn't match semver scheme: 'Wow'",
+        ),
+        # Semver suffix
+        (
+            'invalid_tool = "1.2.3.dev"',
+            "invalid_tool: Version doesn't match semver scheme: '1.2.3.dev'",
+        ),
+        # Epoch prefix (Python specific)
+        (
+            'invalid_tool = "20!1.2.3"',
+            "invalid_tool: Version doesn't match semver scheme: '20!1.2.3'",
+        ),
+        ("invalid_tool = {}", "invalid_tool: Version is mandatory"),
+        ('invalid_tool = {"key" = "value"}', "invalid_tool: Version is mandatory"),
+        ('invalid_tool = {"version" = {}}', "invalid_tool: Version must be a string"),
+        ('invalid_tool = {"version" = true}', "invalid_tool: Version must be a string"),
+        (
+            'invalid_tool = {"version" = false}',
+            "invalid_tool: Version must be a string",
+        ),
+        ('invalid_tool = {"version" = 123}', "invalid_tool: Version must be a string"),
+        (
+            'invalid_tool = {"version" = "Wow"}',
+            "invalid_tool: Version doesn't match semver scheme: 'Wow'",
+        ),
+        (
+            'invalid_tool = {"version" = "1.2.3.dev"}',
+            "invalid_tool: Version doesn't match semver scheme: '1.2.3.dev'",
+        ),
+        (
+            'invalid_tool = {"version" = "20!1.2.3"}',
+            "invalid_tool: Version doesn't match semver scheme: '20!1.2.3'",
+        ),
+    ),
+)
+def test_negative(
+    section_data: str,
+    expected_error: str,
+    tmp_path: pathlib.Path,
+):
+    cargo_home = tmp_path / ".cargo"
+    tool_file = tmp_path / "tools.toml"
+    setup_cargo_home(cargo_home, False)
+    setup_tools_toml(tool_file, section_data, unknown_section=False, raw=False)
+
+    (code, stdout, stderr) = execute_command(
+        cargo_home,
+        tool_file,
+        True,
+        CRATE_DATASOURCE,
+        True,
+    )
+    assert len(stdout) == 0
+    assert code != 0
+
+    for line in stderr.splitlines():
+        if "[DEBUG]" in line or "[INFO]" in line or "[WARNING]" in line:
+            continue
+
+        message = line.split("[ERROR] ", 1)
+
+        assert len(message) == 2
+        assert message[1] == expected_error
